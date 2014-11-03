22dca8c5d44c05743671d6a7e7cbcea8ed67afce
Alexander Kine <sanya_179510@mail.ru>
2014-11-03 23:01:57 +0500

    Добавлены тесты на пустой текст и абзац
    Signed-off-by: Alexander Kine <sanya_179510@mail.ru>

---
+++ mark/mark/MarkdownProcessor_should.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Runtime.InteropServices;
+
+namespace mark
+{
+    [TestFixture]
+    public class MarkdownProcessor_should
+    {
+        private readonly MarkdownProcessor processor = new MarkdownProcessor();
+        [Test]
+        public void convert_empty_text()
+        {
+            var text = "";
+            var result = processor.ConvertFromMarkdownToHtml(text);
+            Assert.AreEqual(result, string.Empty);
+        }
+
+        [Test]
+        public void convert_the_paragraph()
+        {
+            var text = "\n    \nTesting is good!";
+            var result = processor.ConvertFromMarkdownToHtml(text);
+            Assert.AreEqual(result,"<p>Testing is good!</p>");
+        }
+    }
+}
