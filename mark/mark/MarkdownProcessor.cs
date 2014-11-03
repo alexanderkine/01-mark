978a9280c4fe440b7ee9704fed244afef34db8a1
Alexander Kine <sanya_179510@mail.ru>
2014-11-03 23:04:38 +0500

    Код на озеленение теста=)
    Signed-off-by: Alexander Kine <sanya_179510@mail.ru>

---
+++ mark/mark/MarkdownProcessor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace mark
+{
+    public class MarkdownProcessor : IProcessor
+    {
+        //private StringBuilder convertedText;
+        public string ConvertFromMarkdownToHtml(string markdownText)
+        {
+            if (string.IsNullOrEmpty(markdownText)) return string.Empty;
+            var paragraphs = Regex.Split(markdownText, @"\n\s*\n")
+                .Where(s => !string.IsNullOrEmpty(s))
+                .Select(s => string.Format("<p>{0}</p>", s));
+            return string.Join(string.Empty, paragraphs);
+        }
+    }
+}
