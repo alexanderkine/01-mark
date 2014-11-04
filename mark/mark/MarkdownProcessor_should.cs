c6d15a13df872ccf915260918e6c0fb83624fe31
Alexander Kine <sanya_179510@mail.ru>
2014-11-04 16:54:15 +0500

    Добавлен тест
    Signed-off-by: Alexander Kine <sanya_179510@mail.ru>

--- mark/mark/MarkdownProcessor_should.cs
+++ mark/mark/MarkdownProcessor_should.cs
@@ -11,21 +11,34 @@ namespace mark
     [TestFixture]
     public class MarkdownProcessor_should
     {
-        private readonly MarkdownProcessor processor = new MarkdownProcessor();
+        private readonly IProcessor processor = new MarkdownProcessor();
+        
         [Test]
         public void convert_empty_text()
         {
-            var text = "";
-            var result = processor.ConvertFromMarkdownToHtml(text);
-            Assert.AreEqual(result, string.Empty);
+            Test("","");
         }
 
         [Test]
         public void convert_the_paragraph()
         {
             var text = "\n    \nTesting is good!";
-            var result = processor.ConvertFromMarkdownToHtml(text);
-            Assert.AreEqual(result,"<p>Testing is good!</p>");
+            var expectedResult = "<p>Testing is good!</p>";
+            Test(text,expectedResult);
+        }
+
+        [Test]
+        public void convert_the_paragraphs()
+        {           
+            var text = "Haha!\n    \nTesting is good!\n   \nFor everybody programm!\nIt's true";
+            var expectedResult = "<p>Haha!</p><p>Testing is good!</p><p>For everybody programm!\nIt's true</p>";
+            Test(text,expectedResult);
+        }
+
+        private void Test(string text, string result)
+        {
+            var actualResult = processor.ConvertFromMarkdownToHtml(text);
+            Assert.AreEqual(actualResult,result);
         }
     }
 }
